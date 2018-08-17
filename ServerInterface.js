73887db700677f9407c3fa1ff1383ff6a4d3f83a
Akamelia <42076969+Akamelia@users.noreply.github.com>
2018-08-17 22:17:45 +0200

    Update ServerInterface.js

--- ServerInterface.js
+++ ServerInterface.js
@@ -7,6 +7,7 @@ function ServerInterface(bot)
 {
     this.bot = bot;
 }
+console.log(Date.now());
 ServerInterface.prototype.initialize = function() {
     const app = new express();
 
@@ -15,6 +16,21 @@ ServerInterface.prototype.initialize = function() {
 
     app.get('/ping', (req, res) => {
         res.send('ok');
+
+        const guild = this.bot.client.guilds.filter(g => g.name === 'Orokin Guilde Académie').first();
+        if(guild)
+        {
+            const usr = guild.members
+                .map(m => m.user)
+                .filter(u => u.username.indexOf('Akamelia') === 0)// || u.username.indexOf('general_shark') !== -1);
+                [0];
+    
+            console.log(usr.username, Date.now());
+                /*
+            usr.createDM().then(dm => {
+                dm.send(``);
+            })*/
+        }
     });
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'serverInterface.html'));
